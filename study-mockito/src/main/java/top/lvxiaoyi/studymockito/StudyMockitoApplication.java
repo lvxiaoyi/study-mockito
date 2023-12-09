e6a3e19418bf4b0e607b901839a85a2b8dc3193f
lvxiaoyi <lvxiaoyi_lv@163.com>
2023-12-09 22:29:35 +0800

    add:init

---
+++ study-mockito/src/main/java/top/lvxiaoyi/studymockito/StudyMockitoApplication.java
@@ -0,0 +1,13 @@
+package top.lvxiaoyi.studymockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyMockitoApplication.class, args);
+    }
+
+}
---
+++ study-mockito/src/test/java/top/lvxiaoyi/studymockito/StudyMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lvxiaoyi.studymockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
